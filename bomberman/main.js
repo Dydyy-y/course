3b670a4d1481117e65c311049f914d23bc73ee3a
Dydyy-y <210338140+Dydyy-y@users.noreply.github.com>
2025-11-08 11:35:34 +0100

    bomberman class

--- bomberman/main.js
+++ bomberman/main.js
@@ -1,4 +1,4 @@
-const canvasElement = document.getElementById("canvas");
+/* const canvasElement = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 
 canvas.height = 400;
@@ -9,4 +9,19 @@ ctx.fillStyle = 'blue'
 ctx.beginPath();
 ctx.arc(200, 200, 50, 0, 2 * Math.PI)
 
-ctx.fill();
+ctx.fill(); */
+
+const CE = 40;
+const ROWS = 13;
+const COLUMN = 15;
+
+function gridCreation() {
+    for (let row = 0 ; row < ROWS ; row++) {
+        if(row === 0 || row === 13){
+            console.log("W")
+        }
+        for (let col = 0 ; col < COLUMN ; col++){
+            console.log(`${row} ${col}`);
+        }
+    }
+}
