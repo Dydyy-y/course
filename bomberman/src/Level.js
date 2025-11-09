d609403bff4c0f98e97ffbe1f97ed6c9ee0c2ae5
Dydyyy <210338140+Dydyy-y@users.noreply.github.com>
2025-11-09 17:35:43 +0100

    etape 1 done

--- bomberman/src/Level.js
+++ bomberman/src/Level.js
@@ -1,3 +1,4 @@
+
 class Level {
   constructor(width, height, sizeCase) {
     this.columns = width;
@@ -6,31 +7,47 @@ class Level {
     this.grid = [];
 
     this.createGrid();
-    }
+  }
 
   createGrid() {
     for (let row = 0; row < this.rows; row++) {
-        this.grid[row] = [];
+      this.grid[row] = [];
 
-        for (let column = 0; column < this.columns; column++) {
-            if (    //border
-                row === 0 ||
-                row === this.rows - 1 ||
-                column === 0 ||
-                column === this.columns - 1
-            ) {
-                this.grid[row][column] = "W";
-            } 
-            else if (row >= 2 && row % 2 === 0 && column >= 2 && column % 2 === 0) {    //wall grid
-                this.grid[row][column] = "W";
+      for (let column = 0; column < this.columns; column++) {
+        if (
+          //border
+          row === 0 ||
+          row === this.rows - 1 ||
+          column === 0 ||
+          column === this.columns - 1
+        ) {
+          this.grid[row][column] = "W";
+        } else if (
+          row >= 2 &&
+          row % 2 === 0 &&
+          column >= 2 &&
+          column % 2 === 0
+        ) {
+          //wall grid
+          this.grid[row][column] = "W";
+        } else {
+          if (
+            (row === 1 && column === 1) ||
+            (row === 1 && column === 2) ||
+            (row === 2 && column === 1)
+          ) {
+            this.grid[row][column] = ".";
+          } else {
+            if (Math.random() < 0.5) {
+              this.grid[row][column] = "S";
+            } else {
+              this.grid[row][column] = ".";
             }
-            else {
-                this.grid[row][column] = ".";
-            }
+          }
         }
+      }
     }
-}
-
+  }
 }
 
 const level = new Level(15, 13, 40);
