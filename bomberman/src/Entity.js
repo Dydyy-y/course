395cfd25abaf4af301790bf3af89a18995bf7552
Dydyy-y <210338140+Dydyy-y@users.noreply.github.com>
2025-11-10 16:49:22 +0100

    bomberman class

---
+++ bomberman/src/Entity.js
@@ -0,0 +1,13 @@
+class Entity {
+    constructor(x, y) {
+        this.x = x;      
+        this.y = y;      
+        this.isActive = true;
+    }
+    
+    update() {
+    }
+    
+    render(ctx, sizeCase) {
+    }
+}
--- bomberman/src/PowerUp.js
+++ bomberman/src/PowerUp.js
@@ -1,15 +1,21 @@
-class Wall extends Entity {
-    constructor(x, y, destructible) {
-        super(x, y);
-        this.destructible = destructible; // Mur cassable ou non
+class PowerUp extends Entity {
+  constructor(x, y, type) {
+    super(x, y);
+    this.type = type;
+  }
+
+  render(ctx, sizeCase) {
+    if (this.type === "speed") {
+      ctx.fillStyle = "#ffff00";
+    } else if (this.type === "bomb") {
+      ctx.fillStyle = "#008000";
+    } else if (this.type === "range") {
+      ctx.fillStyle = "#0000ff";
     }
-    
-    render(ctx, sizeCase) {
-        // Gris foncé si indestructible
-        // Gris clair si destructible
-    }
-    
-    update() {
-        // Rien (les murs ne bougent pas)
-    }
+
+    ctx.fillRect(this.x * sizeCase, this.y * sizeCase, sizeCase, sizeCase);
+  }
+
+  update() {
+  }
 }
