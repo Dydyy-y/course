eb18ac14a8e1e7de3f0e4475a153f13db389a17e
Dydyy-y <210338140+Dydyy-y@users.noreply.github.com>
2025-11-27 10:53:20 +0100

    dom recherche lancée

---
+++ weather_API/script.js
@@ -0,0 +1,7 @@
+// Récupération des éléments du DOM
+const searchButton = document.getElementById('searchButton');
+
+// Interaction : clic sur le bouton "Rechercher"
+searchButton.addEventListener('click', () => {
+    console.log('Recherche lancée !');
+});
